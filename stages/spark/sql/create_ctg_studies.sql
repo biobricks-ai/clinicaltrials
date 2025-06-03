aeb3f6dc27d63dfb66ad302c5ee9f107e9681209
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-06-03 14:02:05 -0400

    feat: Enable merge-on-read mode for Iceberg table
    Configure the ctg_studies Iceberg table to use merge-on-read mode for
    write, update, and delete operations. This improves performance for
    incremental updates by avoiding expensive rewrites of data files.

--- stages/spark/sql/create_ctg_studies.sql
+++ stages/spark/sql/create_ctg_studies.sql
@@ -4,4 +4,9 @@ CREATE TABLE IF NOT EXISTS local.ctg_studies (
   derived_section_norm_json STRING,
   version_holder_json STRING
 )
-USING iceberg;
+USING iceberg
+TBLPROPERTIES (
+  'write.merge.mode' = 'merge-on-read',
+  'write.update.mode' = 'merge-on-read',
+  'write.delete.mode' = 'merge-on-read'
+);
